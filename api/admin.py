573ea25e6af60190c4ee6ebfb4eb1e870f2f807f
Siddhant Gupta <siddhantgupta@Siddhants-Mac-mini.local>
2025-07-26 00:55:05 +0530

    added filters

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,14 @@ from api.models import Company,Employee
 
 # Register your models here.
 
-admin.site.register(Company)
-admin.site.register(Employee)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location','type')
+    search_fields=('name',)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name','email','position')
+    list_filter=('company',)
+
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)
 
 
